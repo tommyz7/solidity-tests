83a47f8c1cb767f56198abde163fa50c8cd9520f
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-10-10 19:01:11 -0400

    Update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ContractManager = artifacts.require("./ContractManager.sol");
+var ActionManager = artifacts.require("./ActionManager.sol");
+var ActionDB = artifacts.require("./ActionDB.sol");
+var PermissionDB = artifacts.require("./PermissionDB.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+
+module.exports = function(deployer, network, accounts) {
+    deployer.deploy(ContractManager);
+    deployer.deploy(ActionManager);
+    deployer.deploy(ActionDB);
+    deployer.deploy(PermissionDB);
 };
