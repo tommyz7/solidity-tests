e6fa3101ab4855e2eddde23b5648ad55278623cf
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-10-18 17:28:42 -0400

    Add action - Set action permission

---
+++ contracts/Actions/ActionSetActionPermission.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.17;
+
+
+import '../Action.sol';
+
+
+contract ActionSetActionPermission is Action {
+    function execute(bytes32 name, uint8 perm) public onlyActionManager returns(bool) {
+        address actiondb = ContractProvider(CM).contracts("actiondb");
+        address action = ActionDbProvider(actiondb).actions(name);
+        return Action(action).setPermission(perm);
+    }
+}
